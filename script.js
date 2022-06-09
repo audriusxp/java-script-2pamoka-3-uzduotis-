facf096953c2857fd9fcf59ef7736e9c817ac379
audriusxp <palm@inbox.lt>
2022-06-09 10:59:27 +0300

    padaryta pirma uzduoties dalis

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+let zeme = "23saulessistema";
+let dangus = "menulis";
+
+console.log(zeme);
+console.log(dangus);
+let zodziujunginys = sujungtizodziai(zeme,dangus);
+console.log(zodziujunginys);
+
+}
+
+
+function sujungtizodziai(zeme,dangus) 
+{
+    return zeme+dangus;
+}
