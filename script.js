317739cebbdeb77f1d97c0aa77815970966998bc
audriusxp <palm@inbox.lt>
2022-06-10 18:53:46 +0300

    ppabaigta uzduotis

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ window.onload = function() {
 let zeme = "23saulessistema";
 let dangus = "menulis";
 
+
 console.log(zeme);
 console.log(dangus);
-let zodziujunginys = sujungtizodziai(zeme,dangus);
-console.log(zodziujunginys);
+let ilgiusuma = dviejuZodziuIlgis(zeme,dangus);
+
+console.log(ilgiusuma)
 
 }
 
 
-function sujungtizodziai(zeme,dangus) 
+function sujungtizodziai(a,b) 
 {
-    return zeme+dangus;
+    return a+b;
+}
+function dviejuZodziuIlgis(text1,text2){
+    let texstuIlgis = text1.length;
+    let texstu2Ilgis = text2.length;
+    let ilgiusuma = texstuIlgis + texstu2Ilgis;
+
+    return ilgiusuma;
 }
